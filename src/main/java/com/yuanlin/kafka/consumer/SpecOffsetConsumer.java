c9cc559934d6030bdb76c1c6a2c4c3e72c627492
yuanlin <ly_kof@sina.com>
2022-08-12 02:16:17 +0800

    specific offset commit

---
+++ src/main/java/com/yuanlin/kafka/consumer/SpecOffsetConsumer.java
@@ -0,0 +1,62 @@
+package com.yuanlin.kafka.consumer;
+
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.apache.kafka.common.TopicPartition;
+import org.apache.kafka.common.serialization.StringDeserializer;
+
+import java.time.Duration;
+import java.util.ArrayList;
+import java.util.Properties;
+import java.util.Set;
+
+public class SpecOffsetConsumer
+{
+	public static void main(String[] args)
+	{
+		Properties properties = new Properties();
+
+		//1---info configuration
+		properties.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG,"esMaster:9092,HWKMaster:9092");
+		properties.put(ConsumerConfig.GROUP_ID_CONFIG, "fooConsumer");
+
+		//2---deserialization
+		properties.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+		properties.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+
+		//3---Consumer client
+		KafkaConsumer<String, String> kafkaConsumer = new KafkaConsumer<>(properties);
+
+		//4---Topic
+		ArrayList<String> topics = new ArrayList<>();
+		topics.add("foo");
+		kafkaConsumer.subscribe(topics);
+
+		//5--Specific offset
+		Set<TopicPartition> assignment = kafkaConsumer.assignment();
+
+		while(assignment.size() == 0)
+		{
+			kafkaConsumer.poll(Duration.ofSeconds(1));
+			assignment = kafkaConsumer.assignment();
+		}
+
+		for(TopicPartition par:assignment)
+		{
+			kafkaConsumer.seek(par, 5);
+		}
+
+		//6---get data
+		while (true)
+		{
+			ConsumerRecords<String, String> consumerRecords = kafkaConsumer.poll(Duration.ofSeconds(1));
+			for(ConsumerRecord<String, String> consumerRecord:consumerRecords)
+			{
+				System.out.println(consumerRecord);
+			}
+		}
+
+	}
+}
